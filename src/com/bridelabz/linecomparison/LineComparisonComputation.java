8fed6efc40ed8583372bcce6488889ac3a652fcd
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 13:25:03 +0530

    Added : Welcome On Line Comparison program

---
+++ src/com/bridelabz/linecomparison/LineComparisonComputation.java
@@ -0,0 +1,7 @@
+package com.bridelabz.linecomparison;
+
+public class LineComparisonComputation {
+    public static void main(String[] args) {
+        System.out.println("....... Welcome On Line Comparison Program .......");
+    }
+}
