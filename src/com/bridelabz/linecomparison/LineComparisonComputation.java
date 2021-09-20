e215206a0160c2f49b40d3f9cd25f788fadedd20
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 13:31:02 +0530

    MODIFIED : Check Legth Of Line

--- src/com/bridelabz/linecomparison/LineComparisonComputation.java
+++ src/com/bridelabz/linecomparison/LineComparisonComputation.java
@@ -1,7 +1,21 @@
 package com.bridelabz.linecomparison;
 
+import java.util.Random;
+
 public class LineComparisonComputation {
     public static void main(String[] args) {
         System.out.println("....... Welcome On Line Comparison Program .......");
+        Random rand = new Random();
+        int x1 = rand.nextInt(10);
+        int x2 = rand.nextInt(10);
+        int y1 = rand.nextInt(10);
+        int y2 = rand.nextInt(10);
+
+        Double length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("x1 : "+x1);
+        System.out.println("x2 : "+x2);
+        System.out.println("y1 : "+y1);
+        System.out.println("y2 : "+y2);
+        System.out.println("Length of the line is : "+length);
     }
 }
